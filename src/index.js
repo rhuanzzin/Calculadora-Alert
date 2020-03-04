63ccc4c14a25a1a5ffb731d69e234454921b578b
rhuan Gon <rhuan.gon@gmail.com>
2020-03-03 21:20:43 -0300

    calculadora linda
    calc

--- src/index.js
+++ src/index.js
@@ -1,36 +1,23 @@
-/* SINTAXE -  
 
-function nome(parâmetro){
-  corpo da função - lógica
-  return 
-}
 
-invocando - nome()
+function start(){
+  let a = Number(prompt("Escolha Primeiro número"));
+  let op = prompt("Escolha a operação desejada");
+  let b = Number(prompt("Escolha o segundo número"));
 
----------------------------------
-
- function ola() {
-  return document.write(`Olá`);
-} 
-*/
-
-//ola();
-
-let a = Number(prompt("Primeiro numero"));
-let op = prompt("operação");
-let b = Number(prompt("segundo número"));
 
 function soma(a, b) {
-  return document.write(`a soma entre ${a} e ${b} é ${a + b} `);
+  return document.getElementById('resultado').innerHTML = `<h2>a soma entre ${a} e ${b} é ${a + b}<h2>`;
 }
 function sub(a, b) {
-  return document.write(`a subtração entre ${a} e ${b} é ${a - b} `);
+  return document.getElementById('resultado').innerHTML = `<h2>a subtração entre ${a} e ${b} é ${a - b} </h2>`;
+
 }
 function mult(a, b) {
-  return document.write(`a multiplicação entre ${a} e ${b} é ${a * b} `);
+  return document.getElementById('resultado').innerHTML = `<h2>a multiplicação entre ${a} e ${b} é ${a * b}</h2`;
 }
 function divs(a, b) {
-  return document.write(`a divisão entre ${a} e ${b} é ${a / b} `);
+  return document.getElementById('resultado').innerHTML = `<h2>a divisão entre ${a} e ${b} é ${a / b}<h2>`;
 }
 
 switch (op) {
@@ -52,4 +39,5 @@ switch (op) {
 
   default:
     break;
+}
 }
